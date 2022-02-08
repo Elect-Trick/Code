173ad42a692efea03adce2a3dd067d59d3455142
Elect-Trick <snmajenje@gmail.com>
2022-02-08 08:07:18 +0200

    Routing
    -Added angular routers and added routerLink to the app component
    -Generate new components to test our routing

--- DOTNET/client/src/app/Services/account.service.ts
+++ DOTNET/client/src/app/Services/account.service.ts
@@ -6,63 +6,51 @@ import { ReplaySubject } from 'rxjs';
 
 // Services uses the singleTOn pattern, it is only destroyed when we leave the page
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
-  baseUrl = "https://localhost:5001/api/";
-  private currentUserSource = new ReplaySubject<any>(1)
-currentUser$ = this.currentUserSource.asObservable();
-  constructor(private http: HttpClient) { }
+  baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new ReplaySubject<any>(1);
+  currentUser$ = this.currentUserSource.asObservable();
+  constructor(private http: HttpClient) {}
 
-  login(loginData: any)
-  {
-    return this.http.post<User>(this.baseUrl+'account/login', loginData).pipe(
-      map((response: User) =>{
-        const user= response;
-        if(user)
-        {
+  login(loginData: any) {
+    return this.http.post<User>(this.baseUrl + 'account/login', loginData).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
         }
         return user;
-
       })
     );
   }
 
-  register(registrationData: any)
-  {
-    return this.http.post<User>(this.baseUrl+'account/register',registrationData).pipe(
-      map((user:User)=>{
-
-        console.log('User is ', user)
-        if(user)
-        {
-        localStorage.setItem('user', JSON.stringify(user));
-         this.currentUserSource.next(user);
-
-        }
-return user;
-
-      }
-
-    ));
-
+  register(registrationData: any) {
+    return this.http
+      .post<User>(this.baseUrl + 'account/register', registrationData)
+      .pipe(
+        map((user: User) => {
+          if (user) {
+            localStorage.setItem('user', JSON.stringify(user));
+            this.currentUserSource.next(user);
+          }
+          return user;
+        })
+      );
   }
 
-  async getAllUsers()
-  {
-   return await this.http.get<User[]>(this.baseUrl+'users');
-
+  async getAllUsers() {
+    return await this.http.get<User[]>(this.baseUrl + 'users');
   }
 
-  setCurrentUser(user: User){
+  setCurrentUser(user: User) {
     this.currentUserSource.next(user);
   }
 
-  logout()
-  {
-   let user: User;
+  logout() {
+    let user: User;
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
   }
--- DOTNET/client/src/app/app-routing.module.ts
+++ DOTNET/client/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { MessagesComponent } from './messages/messages.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'', component:HomeComponent},
+  {path:'members', component:MemberListComponent},
+  {path:'members/:id', component:MemberDetailComponent},
+  {path:'lists', component:ListsComponent},
+  {path:'messages', component:MessagesComponent},
+  // Wildcard route, when a user enters a non existant resource
+  {path:'**', component:MessagesComponent, pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
