d3221e2b8a25c86fe86437b0135fd35284afc8f5
Elect-Trick <snmajenje@gmail.com>
2022-03-19 14:02:30 +0200

    Identity Service Update

--- DOTNET/BookingApp/BookingApp/Extensions/IdentityServiceExtension.cs
+++ DOTNET/BookingApp/BookingApp/Extensions/IdentityServiceExtension.cs
@@ -20,7 +20,7 @@ namespace API.Extensions
         public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddCors();
-            services.AddIdentityCore<ApplicationUser>(options =>
+            services.AddIdentityCore<IdentityUser>(options =>
             {
                 options.Password.RequireNonAlphanumeric = false;
                 options.Password.RequiredLength = 1;
@@ -31,7 +31,7 @@ namespace API.Extensions
 
             }).AddRoles<AppRole>()
             .AddRoleManager<RoleManager<AppRole>>()
-            .AddSignInManager<SignInManager<ApplicationUser>>()
+            .AddSignInManager<SignInManager<IdentityUser>>()
             .AddRoleValidator<RoleValidator<AppRole>>()
             .AddEntityFrameworkStores<DataContext>().AddDefaultTokenProviders();
 
