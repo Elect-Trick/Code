7331dddd8b765d5d82b2591ea2d7897440570f7c
Elect-Trick <snmajenje@gmail.com>
2022-03-19 14:32:57 +0200

    Data Context Update

--- DOTNET/BookingApp/BookingApp/Data/DataContext.cs
+++ DOTNET/BookingApp/BookingApp/Data/DataContext.cs
@@ -1,14 +1,22 @@
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using API.Entities;
 
 namespace BookingApp.Data
 {
-    public class DataContext : IdentityDbContext<ApplicationUser ,AppRole,int>
+    public class DataContext : IdentityDbContext<ApplicationUser, AppRole, int>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
+
         }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
     }
+
 }
